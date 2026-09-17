c7b2f8eeae3d6a7341fc648cf939d7ab95f18875
GaraevILka <kothvostik@mail.ru>
2026-09-17 15:43:56 +0300

    добавил нумерацию месяцев

--- CALENDAR.cpp
+++ CALENDAR.cpp
@@ -3,44 +3,45 @@
 
 int main(){
     int a;
+    std::cout<<" Choose month"<< std::endl;
     std::cin>> a;
     switch (a)
     {
     case 1:
-        std::cout<<"January"<<std::endl;
+        std::cout<<"1st is January"<<std::endl;
         break;
     case 2:
-       std::cout<<"February"<<std::endl;
+       std::cout<<"2nd is February"<<std::endl;
        break;
     case 3:
-        std::cout<<"March"<<std::endl;
+        std::cout<<"3rd is March"<<std::endl;
         break;
     case 4:
-        std::cout<<"April"<<std::endl;
+        std::cout<<"4th is April"<<std::endl;
         break;
     case 5:
-        std::cout<<"May"<<std::endl;
+        std::cout<<"5th is May"<<std::endl;
         break;
     case 6:
-        std::cout<<"June"<<std::endl;
+        std::cout<<"6th is June"<<std::endl;
         break;
     case 7:
-        std::cout<<"July"<<std::endl;
+        std::cout<<"7th is July"<<std::endl;
         break;
     case 8:
-        std::cout<<"August"<<std::endl;
+        std::cout<<"8th is August"<<std::endl;
         break;
     case 9:
-        std::cout<<"September"<<std::endl;
+        std::cout<<"9th is September"<<std::endl;
         break;
     case 10:
-        std::cout<<"October"<<std::endl;
+        std::cout<<"10th is October"<<std::endl;
         break;
     case 11:
-        std::cout<<"November"<<std::endl;
+        std::cout<<"11th is November"<<std::endl;
         break;
     case 12:
-        std::cout<<"December"<<std::endl;
+        std::cout<<"12th is December"<<std::endl;
         break;
     default:
         std::cout<<" net takogo mesyaca"<<std::endl;
